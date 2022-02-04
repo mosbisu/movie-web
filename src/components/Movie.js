18073aeef9df2b051590be60da9bbf3e484db1e7
kr <hoylfree@gmail.com>
2022-02-04 17:35:24 +0900

    first commit

---
+++ src/components/Movie.js
@@ -0,0 +1,29 @@
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ id, medium_cover_image, title, summary, genres }) {
+    return (
+        <div>
+            <img src={medium_cover_image} alt={title}></img>
+            <h2>
+                <Link to={`/movie/${id}`}>{title}</Link>
+            </h2>
+            <p>{summary}</p>
+            <ul>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    id:propTypes.number.isRequired,
+    medium_cover_image: propTypes.string.isRequired,
+    title: propTypes.string.isRequired,
+    summary: propTypes.string.isRequired,
+    genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default Movie;
